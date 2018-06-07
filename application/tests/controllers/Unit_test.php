1bf1bc837f7ea10def9d40e10ff2b07223b544e5
zola <zola.octanoviar@finansia.com>
2018-06-07 15:15:36 +0700

    Update unit test case

--- application/tests/controllers/Unit_test.php
+++ application/tests/controllers/Unit_test.php
@@ -10,14 +10,11 @@ class Unit_test extends TestCase
 
 	public function test_rate_get()
 	{
-		/*
 		try {
 			$output = $this->request('GET', 'product/rate');
 		} catch (CIPHPUnitTestExitException $e) {
 			$output = ob_get_clean();
 		}
-		*/
-		$output = $this->request('GET', 'product/rate');
 		$this->assertEquals(
 			'[{"id":1,"name":"motor bekas","rate":"5%"},{"id":2,"name":"motor baru","rate":"6%"},{"id":3,"name":"mobil bekas","rate":"7%"},{"id":4,"name":"mobil baru","rate":"8%"},{"id":5,"name":"smartphone","rate":"2%"},{"id":6,"name":"laptop","rate":"2.5%"},{"id":7,"name":"multiguna","rate":"2.5%"}]',
 			$output
@@ -26,9 +23,15 @@ class Unit_test extends TestCase
 		fwrite(STDOUT, __METHOD__ . "\n");
 	}
 
+
 	public function test_rate_get_name()
 	{
-		$output = $this->request('GET', 'product/rate/name/motor');
+		try {
+			$output = $this->request('GET', 'product/rate/name/motor');
+		} catch (CIPHPUnitTestExitException $e) {
+			$output = ob_get_clean();
+		}
+
 		$this->assertEquals(
 			'{"id":2,"name":"motor baru","rate":"6%"}',
 			$output
@@ -37,9 +40,15 @@ class Unit_test extends TestCase
 		fwrite(STDOUT, __METHOD__ . "\n");
 	}
 
+
 	public function test_rate_get_name_failed()
 	{
-		$output = $this->request('GET', 'product/rate/name/samsung');
+		try {
+			$output = $this->request('GET', 'product/rate/name/samsung');
+		} catch (CIPHPUnitTestExitException $e) {
+			$output = ob_get_clean();
+		}
+
 		$this->assertEquals(
 			'{"status":false,"message":"Products could not be found for samsung"}',
 			$output
